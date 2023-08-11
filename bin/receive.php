a11fa7eb48f5d2e9a550624c85a4c13d45376718
Milan Matějček <milan.matejcek@gmail.com>
2023-08-11 20:36:52 +0200

    WIP [ci skip]

--- bin/receive.php
+++ bin/receive.php
@@ -4,19 +4,15 @@ use h4kuna\Queue\Exceptions;
 
 require __DIR__ . '/../src/common.php';
 $queue = createQueue();
-start:
 $queue->restore();
-while (true) {
-	try {
-		$message = $queue->consumer()->receive();
-		logger('received', sprintf('Message: %s, Type: %s', $message->message, $message->type));
-		echo $message->message . PHP_EOL;
-		sleep((int) $message->message);
-	} catch (Exceptions\ReceiveException $e) {
-		logger('msg_receive', $e->getMessage());
-		error_get_last() && logger('msg_receive', error_get_last()['message']);
-		error_clear_last();
-		sleep(1);
-		goto start;
-	}
+
+try {
+	$message = $queue->consumer()->receive();
+	logger('received', sprintf('Message: %s, Type: %s', $message->message, $message->type));
+	echo $message->message . PHP_EOL;
+	sleep((int) $message->message);
+} catch (Exceptions\ReceiveException $e) {
+	sleep(1);
+	throw $e;
 }
+
